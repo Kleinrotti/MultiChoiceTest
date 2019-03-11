aa22f1165863624151df7befe29135428ff7c368
Kleinrotti <kleinrotti@gmail.com>
2019-03-11 11:27:58 +0100

    added serialization

--- Client/Helpers/ControlFillHelper.cs
+++ Client/Helpers/ControlFillHelper.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Client.Helpers
+﻿namespace Client.Helpers
 {
-    static class ControlFillHelper
+    internal static class ControlFillHelper
     {
     }
 }
---
+++ PacketModel/Connection/EventArgs/ClientConnectionChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace PacketModel.Connection.EventArgs
+{
+    internal class ClientConnectionChangedEventArgs : Exception
+    {
+        public TcpClient tcpClient { get; set; }
+
+        public ClientConnectionChangedEventArgs(TcpClient client)
+        {
+            this.tcpClient = client;
+        }
+    }
+}
