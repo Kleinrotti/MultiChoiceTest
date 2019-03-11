a3405aee0189891b59e4099880a9931e50d224e5
Kleinrotti <kleinrotti@gmail.com>
2019-03-11 10:41:41 +0100

    structure

--- PacketModel/Connection/TCPConnector.cs
+++ PacketModel/Connection/TCPConnector.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PacketModel
+namespace PacketModel.Connection
 {
-    public class Class1
+    class TCPConnector
     {
     }
 }
